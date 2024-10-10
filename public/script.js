be1338ff2f8467f597cf29c967ac75688ed99ca7
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-10-09 21:53:49 -0500

    start nav FE mock

--- public/script.js
+++ public/script.js
@@ -1,3 +1,28 @@
+// Header Mock
+const Header = {
+  isActive: false, //document.windowWidth > xxx etc. for the desktop, skipping for now FIX IT IN POST
+
+  elements: {
+    hamburger: document.querySelector('#hamburger'),
+    menu: document.querySelector('header nav')
+  },
+
+  onHamburgerClick: () => {
+    const menuIsHidden = Header.elements.menu.getAttribute('aria-hidden') === 'true';
+    Header.elements.menu.setAttribute('aria-hidden', menuIsHidden ? 'false' : 'true');
+  },
+
+  addEventListeners: () => {
+    Header.elements.hamburger.addEventListener('click', Header.onHamburgerClick);
+  },
+
+  init: () => {
+    Header.addEventListeners();
+  }
+}
+
+// DOM Loaded
 document.addEventListener('DOMContentLoaded', function onDOMLoad() {
   console.log(`-> script.js`);
+  Header.init();
 });

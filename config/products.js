50f98c9509656dfdb2d617cded59bc91e6ded36a
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-11-05 12:59:11 -0600

    continue base styles, continue product planning

--- config/products.js
+++ config/products.js
@@ -9,8 +9,10 @@
 +++ CATEGORIES +++
 1. Classic (colors)
 2. Patterns (patterns ie. plaid/zebra/leopard/checkered/circles)
-4. 
-5. Backers (super special) ie/. mini, mom, dad
+3. Media (better name? culture?) (tv show characters, movie characters, bands, etc)
+4. Seasonal (winter, summer, fall, spring, christmas, halloween, easter, etc)
+5. Sports (nfl, nhl, nba)
+99. Backers (super special) ie/. mini, mom, dad
  */
 
 export const products = {

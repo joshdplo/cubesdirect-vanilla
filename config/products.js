1a9adec14a876c017faa98587b65de0431d32069
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-11-05 14:41:30 -0600

    add new idea for cube customization

--- config/products.js
+++ config/products.js
@@ -7,14 +7,57 @@
 - 
 
 +++ CATEGORIES +++
-1. Classic (colors)
-2. Patterns (patterns ie. plaid/zebra/leopard/checkered/circles)
+1. Classic (colors) - CUSTOMIZABLE
+2. Patterns (patterns ie. plaid/zebra/leopard/checkered/circles) - CUSTOMIZABLE
+
 3. Media (better name? culture?) (tv show characters, movie characters, bands, etc)
 4. Seasonal (winter, summer, fall, spring, christmas, halloween, easter, etc)
 5. Sports (nfl, nhl, nba)
 99. Backers (super special) ie/. mini, mom, dad
  */
 
-export const products = {
+/**
+ * Generate Products
+ * @TODO: fill in descriptions
+ * @TODO: generate slug, images (images will use slug)
+ */
+export const products = [
+  // CLASSIC
+  {
+    name: "Classic Single Color",
+    description: "",
+    price: 4.99,
+    stock: 1000,
+    custom: {
+      colors: 1,
+      engraving: true
+    }
+  },
+  {
+    name: "Classic Custom Colors",
+    description: "",
+    price: 6.99,
+    stock: 1000,
+  },
 
-};
+  // PATTERNS
+  {
+    name: "Plaid",
+    description: "",
+    price: 6.99,
+    stock: 1000,
+  },
+  {
+    name: "Stripes (Horizontal)",
+    description: "",
+    price: 6.99,
+    stock: 1000,
+  },
+  {
+    name: "Stripes (Vertical)",
+    description: "",
+    price: 6.99,
+    stock: 1000,
+  },
+
+];
